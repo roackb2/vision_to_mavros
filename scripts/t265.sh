4f1bed2d1b7aee5fe4bc0edb015838dcad4bcf21
Jay/Fienna Liang <roackb2@gmail.com>
2020-11-24 17:10:16 +0800

    change script location

--- scripts/t265.sh
+++ scripts/t265.sh
@@ -1,6 +1,6 @@
-#!/bin/bash 
-# This is /usr/bin/your_config-start.sh 
-# # do all your commands here... script terminates when all is done. 
+#!/bin/bash
+# This is /usr/bin/your_config-start.sh
+# # do all your commands here... script terminates when all is done.
 
 # Change the path to t265_to_mavlink.py
-/home/ubuntu/catkin_ws/src/vision_to_mavros/scripts/t265_to_mavlink.py
+/home/lj/vision_to_mavros/catkin_ws/src/vision_to_mavros/scripts/t265_to_mavlink.py
