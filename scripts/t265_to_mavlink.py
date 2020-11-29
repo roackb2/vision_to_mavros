f355a9e3ac12d919cd80f6de27bff30ce072c9d8
Jay/Fienna Liang <roackb2@gmail.com>
2020-11-29 18:58:38 +0800

    use two clients to connect to MQTT server

--- scripts/t265_to_mavlink.py
+++ scripts/t265_to_mavlink.py
@@ -381,6 +381,7 @@ def realsense_connect():
     # Start streaming with requested config
     pipe.start(cfg)
 
+
 # Monitor user input from the terminal and update scale factor accordingly
 def scale_update():
     global scale_factor
@@ -406,21 +407,26 @@ def send_image(frame, mqtt_client, topic):
 #######################################
 
 # The callback for when the client receives a CONNACK response from the server.
-def on_connect(client, userdata, flags, rc):
-    print("INFO: Connected to MQTT server with result code "+str(rc))
-    # Subscribing in on_connect() means that if we lose the connection and
-    # reconnect then subscriptions will be renewed.
-    # client.subscribe("$SYS/#")
+def on_direct_connect(client, userdata, flags, rc):
+    print("INFO: Connected to Direct MQTT server with result code "+str(rc))
     client.publish("presence", "drone")
     client.subscribe("command")
 
 # The callback for when a PUBLISH message is received from the server.
-def on_message(client, userdata, msg):
-    print("INFO: message received, topic: " + msg.topic + ", payload: " + msg.payload.decode())
+def on_direct_message(client, userdata, msg):
+    print("INFO: message received from direct, topic: " + msg.topic + ", payload: " + msg.payload.decode())
     if msg.topic == "command":
         cmd = msg.payload.decode("utf-8")
         print("INFO: command received: " + cmd)
         handle_cmd(cmd)
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("INFO: Connected to MQTT server with result code "+str(rc))
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print("INFO: message received, topic: " + msg.topic + ", payload: " + msg.payload.decode())
 
 def handle_cmd(cmd):
     try:
@@ -456,6 +462,10 @@ def handle_cmd(cmd):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+
+direct_client = mqtt.Client()
+direct_client.on_connect = on_direct_connect
+direct_client.on_message = on_direct_message
 
 #######################################
 # Main code starts here
@@ -505,7 +515,12 @@ if compass_enabled == 1:
 print("INFO: Connecting to MQTT server.")
 # client.connect("192.168.2.238", 1883, 10)
 client.connect("10.3.141.1", 1883, 10)
+# client.connect("10.0.0.10", 1883, 10)
 client.loop_start()
+
+print("INFO: Connecting to MQTT server wit direct IP.")
+direct_client.connect("10.3.141.233", 1883, 10)
+direct_client.loop_start()
 
 print("INFO: Sending VISION_POSITION_ESTIMATE messages to FCU.")
 
@@ -521,14 +536,20 @@ try:
             continue
 
         # Wait for the next set of frames from the camera
-        frames = pipe.wait_for_frames()
+        success, frames = pipe.try_wait_for_frames()
+
+        if not success:
+            print("WARNING: frames didn't arrive")
+            pipe.stop()
+            pipe.start()
+            continue
 
         counter = counter + 1
         if counter % 10 == 0:
             print("counter: " + str(counter))
             # fetch imagees
             f1 = frames.get_fisheye_frame(1)
-            f2 = frames.get_fisheye_frame(2)
+            # f2 = frames.get_fisheye_frame(2)
             send_image(f1, client, 'relay/stream/3')
         if counter == 10000:
             counter = 0
